03520603982c2e34d029deeb4215349986a4b1f7
王彤 <wetong.wang@bestwehotel.com>
2019-01-15 19:46:57 +0800

    use mybatis config xml

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -12,26 +12,26 @@ import org.apache.ibatis.annotations.*;
  */
 public interface  UserMapper {
 
-    @Select("select * from users")
-    @Results({
-        @Result(property="userSex",column="user_sex",javaType=UserSexEnum.class),
-        @Result(property = "nickName", column = "nick_name")
-    })
+    // @Select("select * from users")
+    // @Results({
+    //     @Result(property="userSex",column="user_sex",javaType=UserSexEnum.class),
+    //     @Result(property = "nickName", column = "nick_name")
+    // })
     List<UserEntity> getAll();
 
-    @Select("SELECT * FROM users WHERE id = #{id}")
-	@Results({
-		@Result(property = "userSex",  column = "user_sex", javaType = UserSexEnum.class),
-		@Result(property = "nickName", column = "nick_name")
-	})
+    // @Select("SELECT * FROM users WHERE id = #{id}")
+	// @Results({
+	// 	@Result(property = "userSex",  column = "user_sex", javaType = UserSexEnum.class),
+	// 	@Result(property = "nickName", column = "nick_name")
+	// })
 	UserEntity getOne(Long id);
 
-	@Insert("INSERT INTO users(userName,passWord,user_sex,nick_name) VALUES(#{userName}, #{passWord}, #{userSex}, #{nickName})")
+	// @Insert("INSERT INTO users(userName,passWord,user_sex,nick_name) VALUES(#{userName}, #{passWord}, #{userSex}, #{nickName})")
 	void insert(UserEntity user);
 
-	@Update("UPDATE users SET userName=#{userName},nick_name=#{nickName} WHERE id =#{id}")
+	// @Update("UPDATE users SET userName=#{userName},nick_name=#{nickName} WHERE id =#{id}")
 	void update(UserEntity user);
 
-	@Delete("DELETE FROM users WHERE id =#{id}")
+	// @Delete("DELETE FROM users WHERE id =#{id}")
 	void delete(Long id);
 }
